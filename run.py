80215a593c2fdcf0a0ae8b1c61c4342faffd6dac
Daniel Wheeler <daniel.wheeler2@gmail.com>
2015-01-29 11:52:31 -0500

    Fix bug, 'message' key throwing error.

--- run.py
+++ run.py
@@ -3,14 +3,14 @@ import time
 
 config_yaml = 'config.yaml'
 
-for issue_id in range(1, 500):
+for issue_id in range(190, 500):
     while True:
         try:
             bb2gh.migrate(config_yaml, verbose=True, issue_ids=[issue_id])
         except Exception as inst:
             print 'issue_id',issue_id
             print type(inst)
-            print inst.data['message']
+            print inst
             print 'waiting for 60 seconds'
             print
             time.sleep(60)
